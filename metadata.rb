83d078789dc023a6cd2c6c56e968bcaff05c234f
Alexander Pirsig <apirsig@web.de>
2013-11-16 16:21:42 +0100

    bump

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "self@pirsig.net"
 license          "MIT"
 description      "Installs basic ubuntu packages"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 depends          "apt"
 depends          "fail2ban"
