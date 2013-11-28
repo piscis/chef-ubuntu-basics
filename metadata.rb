b293032e94c680ea5af86bb7752f3ddad80296fe
Alex <self@pirsig.net>
2013-11-28 13:47:24 +0100

    Update metadata.rb
    Bump to 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "self@pirsig.net"
 license          "MIT"
 description      "Installs basic ubuntu packages"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "1.1.0"
 depends          "apt"
 depends          "fail2ban"
