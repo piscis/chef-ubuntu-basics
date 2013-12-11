b3ff229980dd904e839f2cdf22226bea9269f6f7
Alex <self@pirsig.net>
2013-12-11 13:10:41 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,8 @@ maintainer_email "self@pirsig.net"
 license          "MIT"
 description      "Installs basic ubuntu packages"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.2"
+version          "1.1.3"
 depends          "apt"
 depends          "fail2ban"
+
+supports "ubuntu"
